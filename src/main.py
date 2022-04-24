629947b274c8b46fd0f1d1da3029d3557d5d995b
Emily Y <emilyyst19@hotmail.com>
2022-04-24 10:42:19 -0400

    Store generated posts
    - decrease response time by generating multiple posts beforehand and giving them to the user on request
    - untested

--- src/main.py
+++ src/main.py
@@ -1,34 +1,84 @@
 from flask import Flask, jsonify
 from flask_cors import CORS, cross_origin
 from generate_unconditional_samples import generate_post
+import json
+from threading import Thread
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
 
 TITLE_DELIMITER = "<|endoftitle|>"
 ENTRY_DELIMITER = "<|endoftext|>"
+POSTS_PATH = "saved_posts.json"
+
+def refill_posts():
+  print("generating more posts...")
+  
+  saved_posts = []
+  with open(POSTS_PATH, "r") as f:
+    saved_posts = json.load(f)
+  
+  if not saved_posts:
+    saved_posts = []
+  
+  while len(saved_posts) < 20:
+    raw_posts = generate_post()
+    raw_posts = raw_posts.split(ENTRY_DELIMITER)
+
+    for p in raw_posts:
+      p = p.split(TITLE_DELIMITER)
+      title = ""
+      body = ""
+      if len(p) > 1:
+        title = p[0]
+        body = "".join(p[1:])
+      else:
+        title = "idk"
+        body ="".join(p)
+      
+      saved_posts.append({
+        "title": title,
+        "body": body
+      })
+    
+    print("number of saved posts:", len(saved_posts))
+  
+  with open(POSTS_PATH, "w") as f:
+    json.dump(saved_posts, f)
+
+def get_post():
+  saved_posts = []
+  with open(POSTS_PATH, "r") as f:
+    saved_posts = json.load(f)
+  
+  post = saved_posts[0]
+  saved_posts = saved_posts[1:]
+
+  with open(POSTS_PATH, "w") as f:
+    json.dump(saved_posts, f)
+  
+  return post
+
 
 @app.route("/generate", methods=['GET'])
 @cross_origin(supports_credentials=True)
 def generate():
-  # call on the model to generate a sample, and return the result
-  # we can separate the title and body here as well, before sending it to the client
 
-  post = generate_post()
-  post = post.split(TITLE_DELIMITER)
+  post = get_post()
+  
+  thread = Thread(target=refill_posts)
+  thread.start()
 
-  title = ""
-  body = ""
-  if len(post) > 1:
-    title = post[0]
-    body = "".join(post[1:])
-  else:
-    body ="".join(post)
+  return jsonify(post)
+  
 
-  return jsonify(
-    title=title,
-    body=body
-  )
+@app.route("/refillllllllllllllllllllllllllllllllll", methods=['GET'])
+@cross_origin(supports_credentials=True)
+def generate():
+  thread = Thread(target=refill_posts)
+  thread.start()
+
+  return "generating posts"
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=105)
